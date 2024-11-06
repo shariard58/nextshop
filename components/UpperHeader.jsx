75e586b0270a15a6abb4293cfbd870700574cc32
shariard58 <shariard58@gmail.com>
2024-11-06 08:48:38 +0600

    uppder Header

--- components/UpperHeader.jsx
+++ components/UpperHeader.jsx
@@ -1,10 +1,10 @@
 import { IoIosArrowUp } from "react-icons/io";
 export default function UpperHeader() {
   return (
-    <div className="grid md:grid-cols-4 gap-[0px] bg-[#F0F1F1] py-[6px]">
+    <div className="grid md:grid-cols-5 gap-[0px] bg-[#F0F1F1] py-[6px]">
       <div className="col-span-1"></div>
-      <div className="md:col-span-2 flex justify-between cursor-pointer">
-        <div className="flex gap-[32px]">
+      <div className="md:col-span-3 flex justify-between cursor-pointer px-[12px]">
+        <div className="flex gap-[28px]">
           <div className="flex gap-[4px]">
             <p className="text-[12px] text-[#F97316]">English</p>
             <IoIosArrowUp color="orange" size={16} />
