8ec649df9256723387c92f684ca0c8a2c1f279cc
shariard58 <shariard58@gmail.com>
2024-11-07 04:24:12 +0600

    signup form

--- app/signup-login/page.jsx
+++ app/signup-login/page.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client";
+import Image from "next/image";
 import { useState } from "react";
 import { FaRegEyeSlash } from "react-icons/fa6";
 import { IoEyeOutline } from "react-icons/io5";
@@ -50,21 +51,20 @@ export default function page() {
               <div className="relative">
                 <input
                   type={visiblePassword ? "text" : "password"}
-                  name=""
                   id="password"
-                  className="border border-gray-300 p-2 w-full"
+                  className="border border-gray-300 p-2 w-full pr-10"
                   placeholder="Please enter your password"
                 />
                 {visiblePassword ? (
                   <IoEyeOutline
                     onClick={() => setVisiblePassword(!visiblePassword)}
-                    className="absolute right-2 top-1/2  cursor-pointer"
+                    className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
                     size={20}
                   />
                 ) : (
                   <FaRegEyeSlash
                     onClick={() => setVisiblePassword(!visiblePassword)}
-                    className="absolute right-2 top-1/2 transform -translate-y-1/2  cursor-pointer"
+                    className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
                     size={20}
                   />
                 )}
@@ -78,22 +78,64 @@ export default function page() {
               >
                 Confirm Password
               </label>
-              <div>
+              <div className="relative">
                 <input
-                  type="password"
+                  type={visibleConfirmPassword ? "text" : "password"}
                   name=""
                   id="confirmPassword"
                   className="border border-gray-300 p-2 w-full"
                   placeholder="Confirm password"
                 />
+
+                {visibleConfirmPassword ? (
+                  <IoEyeOutline
+                    onClick={() =>
+                      setVisibleConfirmPassword(!visibleConfirmPassword)
+                    }
+                    className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                    size={20}
+                  />
+                ) : (
+                  <FaRegEyeSlash
+                    onClick={() =>
+                      setVisibleConfirmPassword(!visibleConfirmPassword)
+                    }
+                    className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                    size={20}
+                  />
+                )}
               </div>
             </div>
           </div>
         </div>
 
-        <div className="space-y-4 md:flex-1 border border-black">
-          <FaRegEyeSlash size={20} />
-          <IoEyeOutline size={20} />
+        <div className="space-y-4 md:flex-1 gap-[10px]">
+          <p className="text-[18px] md:text-[20px] text-[#F97316] font-bold text-end">
+            Login
+          </p>
+
+          <button className="w-full border-none outline-none bg-[#F97316] transition duration-200 ease-in-out hover:bg-orange-400">
+            <p className="text-white text-[16px] font-medium p-[16px]">
+              Sign Up
+            </p>
+          </button>
+          <p className="text-center text-[16px] font-medium text-[#707070]">
+            Or
+          </p>
+
+          <button className="w-full  outline-none border-[2px] border-[#F97316] transition duration-200 ease-in-out  flex items-center justify-center space-x-[0px]">
+            <Image src="/images/facebook.png" alt="fb" height={32} width={32} />
+            <p className="text-[16px] font-medium p-[16px]">
+              Sign Up with Facebook
+            </p>
+          </button>
+
+          <button className="w-full  outline-none border-[2px] border-[#F97316] transition duration-200 ease-in-out  flex items-center justify-center space-x-[0px]">
+            <Image src="/images/google.png" alt="fb" height={32} width={32} />
+            <p className="text-[16px] font-medium p-[16px]">
+              Sign Up with Google
+            </p>
+          </button>
         </div>
       </form>
     </div>
