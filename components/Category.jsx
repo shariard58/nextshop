be8441958bd8ade7ac14b397b5086944329d7e4a
shariard58 <shariard58@gmail.com>
2024-11-07 02:14:49 +0600

    category feature for big screen done so far

--- components/Category.jsx
+++ components/Category.jsx
@@ -28,175 +28,15 @@ export default function Category() {
           "https://api.shope.com.bd/api/v1/public/hero-categories"
         );
         const apiData = await response.json();
-        console.log("the response data is");
         setData(apiData);
       } catch (err) {
         console.error("Failed to fetch data");
       }
     };
+
+    fetchAllDatas();
   }, []);
-  const demoCategory = [
-    {
-      id: 1,
-      title: "Women's & Girls Fashion",
-      parent_id: null,
-      category_id: 14,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/Women-&-Girls-Fashion.svg",
-      link: "/womens-girls-fashion",
-    },
-    {
-      id: 2,
-      title: "Health & Beauty",
-      parent_id: null,
-      category_id: 16,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/Health-&-Beauty.svg",
-      link: "/health-beauty",
-    },
-    {
-      id: 3,
-      title: "Watches, Bags, Jewellery",
-      parent_id: null,
-      category_id: 10,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/Watches-Bags-jewellery.svg",
-      link: "/watches-bags-jewellery",
-      childrens: [
-        {
-          id: 15,
-          title: "kids bags",
-          parent_id: 3,
-          category_id: 1,
-          image: null,
-          link: "/watches-bags-jewellery/kids-bags",
-          childrens: [
-            {
-              id: 19,
-              title: "Bags ABC",
-              parent_id: 15,
-              category_id: 16,
-              image: null,
-              link: "/watches-bags-jewellery/kids-bags/bags-abc",
-            },
-          ],
-        },
-        {
-          id: 16,
-          title: " ravel Bags",
-          parent_id: 3,
-          category_id: 15,
-          image: null,
-          link: "/watches-bags-jewellery/ravel-bags",
-          childrens: [
-            {
-              id: 17,
-              title: "Children Bags",
-              parent_id: 16,
-              category_id: 16,
-              image: null,
-              link: "/watches-bags-jewellery/ravel-bags/children-bags",
-              childrens: [
-                {
-                  id: 18,
-                  title: "Bags 123",
-                  parent_id: 17,
-                  category_id: 16,
-                  image: null,
-                  link: "/watches-bags-jewellery/ravel-bags/children-bags/bags-123",
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 4,
-      title: "Mens & Boys Fashion",
-      parent_id: null,
-      category_id: 5,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/mens-boys-fashion.svg",
-      link: "/mens-boys-fashion",
-    },
-    {
-      id: 5,
-      title: "Mother & Baby",
-      parent_id: null,
-      category_id: 4,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/mother-baby.svg",
-      link: "/mother-baby",
-    },
-    {
-      id: 6,
-      title: "Electronics Devices",
-      parent_id: null,
-      category_id: 10,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/electronics.svg",
-      link: "/electronics-devices",
-    },
-    {
-      id: 7,
-      title: "TV & Home Appliances",
-      parent_id: null,
-      category_id: 16,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/tv-home.svg",
-      link: "/tv-home-appliances",
-    },
-    {
-      id: 8,
-      title: "Electronic Accessories",
-      parent_id: null,
-      category_id: 10,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/electronic-acces.svg",
-      link: "/electronic-accessories",
-    },
-    {
-      id: 9,
-      title: "Groceries",
-      parent_id: null,
-      category_id: 2,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/grocery.svg",
-      link: "/groceries",
-    },
-    {
-      id: 10,
-      title: "Home & Lifestyle",
-      parent_id: null,
-      category_id: 9,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/home-lifestyle.svg",
-      link: "/home-lifestyle",
-    },
-    {
-      id: 11,
-      title: "Sports & Outdoors",
-      parent_id: null,
-      category_id: 15,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/sports.svg",
-      link: "/sports-outdoors",
-    },
-    {
-      id: 12,
-      title: "Automobile",
-      parent_id: null,
-      category_id: 7,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/auto-mobile.svg",
-      link: "/automobile",
-    },
-    {
-      id: 13,
-      title: "Computer and laptop",
-      parent_id: null,
-      category_id: 6,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/computer-laptop.svg",
-      link: "/computer-and-laptop",
-    },
-    {
-      id: 14,
-      title: "Pet Supplies",
-      parent_id: null,
-      category_id: 15,
-      icon: "https://shope.sgp1.cdn.digitaloceanspaces.com/images/hero-categories/icons/pet-supplier.svg",
-      link: "/pet-supplies",
-    },
-  ];
+
   return (
     <div className="relative ">
       <button
@@ -206,14 +46,14 @@ export default function Category() {
         Category
       </button>
 
-      {showCategories && demoCategory && demoCategory.length > 0 && (
+      {showCategories && data && data.length > 0 && (
         <div className="flex absolute top-0 left-0 transition-opacity duration-200 mt-[-2.5px]">
           <div
             onMouseLeave={handleHideCategory}
             className="bg-white shadow-md sm:w-[180px] md:w-[240px] lg:w-[270px] py-2 px-[2px]"
           >
             <div className="border-r-[2px] border-r-gray-300">
-              {demoCategory.map((category) => (
+              {data.map((category) => (
                 <div key={category?.id} className="py-[4px] relative">
                   <div
                     onMouseEnter={() => setHoveredCategory(category)}
