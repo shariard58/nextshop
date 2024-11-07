39d0534755857caef221aac19e21fdd972ad8c81
shariard58 <shariard58@gmail.com>
2024-11-08 02:40:58 +0600

    demo data for products

--- utils/data.js
+++ utils/data.js
@@ -19,4 +19,39 @@ const months = [
   { value: "12", month: "December" },
 ];
 
-export { months, yearArray };
+const productsData = [
+  {
+    id: 1,
+    name: "Moisturizer",
+    imageUrl: "/products/product1.jpg",
+    description:
+      "This is a good mosturizer for all aged people. This is used in winter specially",
+    price: "5000",
+  },
+  {
+    id: 2,
+    name: "Mirrorless Camera",
+    imageUrl: "/products/product2.jpg",
+    description:
+      "High Defination Camera with extra Lense. Good For Low Light Photograph as well. Two years warenty",
+    price: "25000",
+  },
+  {
+    id: 3,
+    name: "Wrist Watch",
+    imageUrl: "/products/product3.jpg",
+    description:
+      "Luxary watch for men. Run in Solar system . 5 Years replace garenty",
+    price: "50000",
+  },
+  {
+    id: 4,
+    name: "Perfume",
+    imageUrl: "/products/product4.jpg",
+    description:
+      "This is luxary purfume from Paris. This is renouned in worldwide for it's haritage",
+    price: "3500",
+  },
+];
+
+export { months, productsData, yearArray };
